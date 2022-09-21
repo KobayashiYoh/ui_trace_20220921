90f4994c80362cd7efdd0dd085594817c260c8c5
KobayashiYoh <y.kobayashi@4nonome.com>
2022-09-21 16:13:28 +0900

    AppColorクラスを作成

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color background = Color(0xFF292E36);
+  static const Color itemBackground = Color(0xFF39414A);
+  static const Color accent = Color(0xFFD3E638);
+}
