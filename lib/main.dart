50c33300ddbe67973d2809f1d65adbb4572c7018
KobayashiYoh <y.kobayashi@4nonome.com>
2022-09-21 14:52:38 +0900

    ボトムナビゲーションバーを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_trace_20220921/views/bottom_navigation_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const BottomNavigationView(),
     );
   }
 }
---
+++ lib/views/bottom_navigation_view.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:ui_trace_20220921/pages/battle_page.dart';
+
+class BottomNavigationView extends StatefulWidget {
+  const BottomNavigationView({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationView> createState() => _BottomNavigationViewState();
+}
+
+class _BottomNavigationViewState extends State<BottomNavigationView> {
+  final List<Widget> _widgetOptions = <Widget>[
+    Container(),
+    Container(),
+    const BattlePage(),
+    Container(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'スケジュール',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'バトル',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'サーモンラン',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.yellow,
+        unselectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
